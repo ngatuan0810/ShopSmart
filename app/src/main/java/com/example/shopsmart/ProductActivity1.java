cf9ac0786cfdb5f09133d226f1afb02a89682f4f
Khanh Nguyen <21072943@students.ltu.edu.au>
2024-05-19 04:03:24 +1000

    add files

--- app/src/main/java/com/example/shopsmart/ProductActivity1.java
+++ app/src/main/java/com/example/shopsmart/ProductActivity1.java
@@ -307,24 +307,7 @@ public class ProductActivity1 extends AppCompatActivity {
             }
             adapter.notifyDataSetChanged();
         }
-        else if (filterType != null && filterType.equals("Fridge")) {
-            filteredList.clear();
-            for (Product product : productList) {
-                if (product.getType().equalsIgnoreCase("Fridge")) {
-                    filteredList.add(product);
-                }
-            }
-            adapter.notifyDataSetChanged();
-        }
-        else if (filterType != null && filterType.equals("Earphones")) {
-            filteredList.clear();
-            for (Product product : productList) {
-                if (product.getType().equalsIgnoreCase("Earphones")) {
-                    filteredList.add(product);
-                }
-            }
-            adapter.notifyDataSetChanged();
-        }
+
     }
 
     private void clearFilters() {
